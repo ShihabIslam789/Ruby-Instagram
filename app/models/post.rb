d7163cd950fbba4ce35b324cf49da23cd4de526d
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-20 13:34:46 -0400

    validate post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true, length: {minimum: 5, maximum: 100}
+    validates :description, prescence: true, length: {minimum: 5, maximum: 500}
+    validates :keywords, prescence: true, length: {minimum: 5, maximum: 100}
+        
+    end
+    
 end
