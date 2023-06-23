6f07185391a4dc428b22c7541ac2b580c29e96ee
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-23 00:28:32 -0400

    user number.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :posts
+
+         before_create : randomizse_id
+         private
+         def randomizse_id
+            begin
+              self.id = Securerandom.random_number
+              (1_000_000_000)
+            end ehile User.where(id: self.id).exists?
+              
+            end
 end
