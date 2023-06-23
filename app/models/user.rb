0b102e5d9991f36b4ea00738b45720a821a72ed8
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-23 18:34:05 -0400

    avatar pic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :posts
-
+         has_many :comments
+         has_one_attached :avatar
          before_create :randomize_id
          private
          def randomize_id
