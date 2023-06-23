79b82cc84f1296d1607f9e0b3bf9cd913099bf1e
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-22 22:40:26 -0400

    fixed errors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_scrape :user do
+  devise_scope :user do
     get '/users', to: 'devise/registrations#new'
     get '/users/password', to: 'devise/passwords#new'
     get '/users/sign_out' =>  'devise/sessions#destroy'
--- db/migrate/db/migrate/20221119111417_add_userid_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddUseridToPosts < ActiveRecord::Migration[7.0]
-    def change
-      add_column :posts, :user_id, :string
-    end
-  end
