3d7638d6beece6e7eefb555ebd8d2094228cc2e6
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-25 14:10:31 -0400

    searchbar function.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-          followablility
-
+        followability
          has_many :posts
          has_many :comments
          has_many :likes
